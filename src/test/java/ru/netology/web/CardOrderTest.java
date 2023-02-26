fdd837612d30102b4d1335755e16d0654c15f5e4
taya sapurina <sapurina0t@gmail.com>
2023-02-26 22:51:32 +0500

    fixed

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -24,9 +24,9 @@ public class CardOrderTest {
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
